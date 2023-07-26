cc98e2a214dcdde477f3f33bd52075a3711c0d7a
Pegu-Mrigen <mrig8520@gmail.com>
2023-07-26 13:33:26 +0530

    front end ended

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const MenuPage = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem) ] flex flex-col md:flex-row items-center">
+    <div className="w-full p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem) ] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
           href={`/menu/category.slug`}
@@ -12,9 +12,9 @@ const MenuPage = () => {
           style={{ backgroundImage: `url(${category.img})` }}
           className="w-full h-1/3 bg-cover p-8 md:h-1/2"
         >
-          <div className={`text-${category.color} w-1/2`}>
+          <div className={`text-${category.color} w-1/2 p-4`}>
             <h1 className="uppercase font-bold text-2xl">{category.title}</h1>
-            <p className="text-sm  lg:text-sm my-6">{category.desc}</p>
+            <p className="text-sm lg:text-sm my-4">{category.desc}</p>
             <button
               className={`hidden 2xl:block bg-${category.color} text-${category.color==="black"? "white":"red-500"} py-2 px-4 rounded-md`}
             >
